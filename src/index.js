548494f099a2b0f114c7e7cc0789c7033b56f616
chadrick <chadrick.kwag@gmail.com>
2018-04-22 14:46:11 +0900

    line example work confirmed

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import Chart from './components/Chart';
+ 
+ReactDOM.render(
+  <Chart />,
+  document.getElementById('root'),
+);
